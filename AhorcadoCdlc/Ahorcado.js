e7d36eca6a4a68100985503bd306b5de9c692b83
cdelacruz-2019262 <cdelacruz-2019262@kinal.edu.gt>
2024-01-19 09:02:44 -0600

    4to commit

--- AhorcadoCdlc/Ahorcado.js
+++ AhorcadoCdlc/Ahorcado.js
@@ -9,7 +9,12 @@ function lienzo(){
     contexto.lineTo(180, 180);
     contexto.strokeStyle = "#f00";
     contexto.stroke();
+    contexto.moveTo(90, 190);
+    contexto.lineTo(140, 120);
+    contexto.strokeStyle = "BLACK";
+    contexto.stroke();
 }
+
 function inicio(){
     pElegida = palabra[Math.floor(Math.random() * palabra.length)];
     letrasCorrectas = [];
