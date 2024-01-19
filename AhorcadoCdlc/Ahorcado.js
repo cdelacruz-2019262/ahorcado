1d4af5703f60246032f49e7651a9f388437b6793
jquevedo-2022151 <informatica@alumno.local>
2024-01-19 07:39:35 -0600

    primer push

---
+++ AhorcadoCdlc/Ahorcado.js
@@ -0,0 +1,8 @@
+window.onload = function(){
+    var canvas = document.getElementById('micanvas');
+    var contexto = canvas.getContext("2d");
+    contexto.moveTo(50, 150);
+    contexto.lineTo(180, 180);
+    contexto.strokeStyle = "#f00";
+    contexto.stroke();
+}
